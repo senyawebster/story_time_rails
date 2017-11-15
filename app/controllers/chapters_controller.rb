05eb119c8bb1696e566d6636f620b846095ff9ce
Nate McGregor and Senya Webster <nate@>
2017-11-15 10:56:26 -0800

    set up seeds, server, and index page

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -1,2 +1,54 @@
 class ChaptersController < ApplicationController
+  def new
+    @story = Story.find(params[:story_id])
+    @chapter = @story.chapters.new
+  end
+
+  def index
+    @story = Story.find(params[:id])
+    @chapters = @story.chapters
+  end
+
+  def show
+    @chapter = Chapter.find(params[:id])
+    @lessons = @chapter.lessons
+  end
+
+  def create
+    @story = Story.find(params[:story_id])
+    @chapter = @story.chapters.new(chapter_params)
+    if @chapter.save
+      redirect_to story_path(@chapter.story)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @story = Story.find(params[:story_id])
+    @chapter = Chapter.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @chapter = Chapter.find(params[:id])
+    @story = @chapter.story
+    if @chapter.update(chapter_params)
+      redirect_to story_path(@story)
+    else
+      render 'chapters/edit'
+    end
+  end
+
+  def destroy
+    @story = Story.find(params[:story_id])
+    @chapter = Chapter.find(params[:id])
+    @chapter.destroy
+    redirect_to story_path(@story)
+  end
+
+  private
+    def chapter_params
+      params.require(:chapter).permit(:sentence, :author, :pic, :story_id)
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+story_list = [
+  "Liar, liar, plants on fire",
+  "If I fits, I sits -Human edition",
+  "Minion Da Club"
+]
+
+story_list.each do |title|
+  Story.create( title: title)
+end
+
+
+chapter_list = [
+  [ "Jane was a liar ,liar and plants were on fire.", "Jane k Rowling", "https://i.imgur.com/CHxqspq.jpg", 1 ],
+  [ "So she got rid of her plants and turned into a spirit", "Miyazaki", "https://i.redditmedia.com/7LWvyyVZovCeyLshMLU9vlLNCLcuH_7HH8uukwkXq8s.png?w=510&s=85e570707bd7d1b17cee30056b918894", 1 ],
+  [ "Plants need energy, so it's on the house- no charge", "Steve Stevenson", "https://i.redditmedia.com/wjld4Q2KKlVCEwMr3wrtxGqEipOssGbF1r9M4kHiHm8.png?w=580&s=60a4207fb414f4efe1c6175262c734a1", 1 ],
+  [ "He found a competitive grave digging competition while surfing google maps", "Jon Jingle-Heimer Schmit", "https://i.redditmedia.com/Y2GnMNByUFKPgMdP137JdakYt99ILt9bvwPXDDkhaWs.png?w=500&s=d96a347eb84fd9313ce0b6c129db69ed", 2 ],
+  [ "Its tough work digging graves, so I filled a pool with bottled water to cool off", "8745.3 human.exe", "https://i.redditmedia.com/vj0zHc6I1IxSVOJJE13IHitgpAJI75XALJ1IsSHFehI.jpg?w=640&s=bfdfd4cb1647c887fbee8f324b026369", 2 ],
+  [ "The water had a rare contaminate that caused my body to sprout carrots, which in turn led the ravenous rabbits to my doom", "C.S. Lewis", "https://i.imgur.com/uXRBY5m.jpg", 2 ],
+  [ "Minions, they just want to have fun!", "Cyndi Lauper", "https://i.imgur.com/cOd4SQA.jpg", 3 ],
+  [ "Rainbow lord the god of cows appeared in da club to expand everyones mind.", "Rainbow Lord", "https://i.imgur.com/y4sPOL9.jpg", 3 ],
+  [ "After a good night out a rest in my hammock is well deserved", "Todd Larson", "https://i.redditmedia.com/FxYRzKBBsK-CrMEa43yDSiXgz69IbAsrRc-XNDGSjv0.jpg?w=633&s=a7a389aea416296bd85f12c49a862b49", 3 ]
+]
+
+chapter_list.each do |sentence, author, pic, story_id|
+  Chapter.create( sentence: sentence,  author: author, pic: pic, story_id: story_id )
+end
